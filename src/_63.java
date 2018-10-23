cb16e7f32411407b8cd7ae58126c736e04122f00
wangze <wangze0801@126.com>
2018-10-23 18:50:46 +0800

    init

---
+++ src/_63.java
@@ -0,0 +1,39 @@
+/**
+ * @author wangz
+ * @date 2018/10/22
+ */
+public class _63 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int b[][] = new int[m][n];
+        b[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 1; i <= n + m - 2; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j > m - 1 || i - j > n - 1) {
+                    continue;
+                }
+                if (obstacleGrid[j][i - j] == 0) {
+                    if (j == 0) {
+                        b[j][i - j] = b[j][i - j - 1];
+                    } else if (j == i) {
+                        b[j][i - j] = b[j - 1][i - j];
+                    } else {
+                        b[j][i - j] = b[j - 1][i - j] + b[j][i - j - 1];
+                    }
+                } else {
+                    b[j][i - j] = 0;
+                }
+            }
+        }
+        return b[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        _63 test = new _63();
+        int a[][] = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        System.out.println(test.uniquePathsWithObstacles(a));
+    }
+
+
+}
---
+++ src/_910.java
@@ -0,0 +1,27 @@
+import java.sql.Struct;
+import java.util.*;
+
+/**
+ * @author wangz
+ * @date 2018/10/22
+ */
+public class _910 {
+    public static int smallestRangeII(int[] A, int K) {
+       Arrays.sort(A);
+       int n = A.length;
+       int min = A[0];
+       int max = A[n-1];
+       int res = max-min;
+        for (int i = 0; i < n-1; i++) {
+            max = Math.max(max,A[i]+2*K);
+            min = Math.min(A[0]+2*K,A[i+1]);
+            res = Math.min(res,max-min);
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        int[] A = {1};
+        System.out.println(smallestRangeII(A,0));
+    }
+
+}
