f83abfbecc44ed2c3e96e143ec1fb177ec212bf7
wangze <wangze0801@126.com>
2018-10-24 08:46:55 +0800

    加了前7个题的题目描述

--- src/_777.java
+++ src/_777.java
@@ -1,10 +1,14 @@
 /**
- * Swap Adjacent in LR String
+ * 在LR字符串中交换相邻字符（Swap Adjacent in LR String）
+ * <p>
+ * 在一个由 'L' , 'R' 和 'X' 三个字符组成的字符串（例如"RXXLRXRXL"）中进行移动操作。
+ * 一次移动操作指用一个"LX"替换一个"XL"，或者用一个"XR"替换一个"RX"。
+ * 现给定起始字符串start和结束字符串end，请编写代码，当且仅当存在一系列移动操作使得start可以转换成end时， 返回True。
  *
- * @author wangze
  * @date 2018/10/19
  */
 public class _777 {
+    //从左到右对比start和end的R，如果相等继续，不相等，如果遇到start的R和end的X，则一直把R往后找，找到遇到L或也是R时交换
     public boolean canTransform(String start, String end) {
         if (start.length() != end.length()) {
             return false;
