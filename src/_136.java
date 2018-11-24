a7bc0d019b869a39c3ecd534ad66454593123de5
wangze <wangze0801@126.com>
2018-11-24 15:17:48 +0800

    注释以及bug修复

--- src/_136.java
+++ src/_136.java
@@ -6,6 +6,7 @@
 public class _136 {
 
     //利用异或计算的性质  a^b=b^a  0^a=a
+    //时间复杂度O(n) 空间复杂度O(1)
     public int singleNumber(int[] nums) {
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
