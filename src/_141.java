64328880bb57bb3599927c147edd7def3298c992
wangze <wangze0801@126.com>
2018-11-26 19:55:21 +0800

    141 环形链表 ：快慢双指针

---
+++ src/_141.java
@@ -0,0 +1,34 @@
+/**
+ * 环形链表(Linked List Cycle)
+ */
+public class _141 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
