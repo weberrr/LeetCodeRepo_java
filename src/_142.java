8a45fc5cb57a4ac2af5ecdb680228cbb31306066
wangze <wangze0801@126.com>
2018-11-26 20:35:21 +0800

    142 环形链表2 ：快慢双指针

---
+++ src/_142.java
@@ -0,0 +1,42 @@
+/**
+ * 环形链表 II (Linked List Cycle  II)
+ *
+ * @date 2018/11/26
+ */
+public class _142 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    //快慢双指针，注意快慢指针时间关系等式： (a+b+n*r)/2 = (a+b)/1 其中b+x=r（环长度）
+    //可以得到 a = x + (n-1)r
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        else if (head.next == head) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
