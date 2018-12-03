54c81b8a3693c9cf935a66a3f4822088e48adaa0
wangze <wangze0801@126.com>
2018-12-03 20:08:37 +0800

    437 路径总和 III : 深度优先搜索变形

--- src/_437.java
+++ src/_437.java
@@ -5,12 +5,34 @@
  */
 public class _437 {
     public class TreeNode {
-      int val;
-     TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-  }
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int num = 0;
+
     public int pathSum(TreeNode root, int sum) {
-return 1;
+        boolean flag = false;
+        dfs(root, sum, flag);
+        return num;
+    }
+
+    private void dfs(TreeNode node, int sum, boolean start) {
+        if (node != null) {
+            if (node.val == sum) {
+                num++;
+            }
+            if (!start) {
+                dfs(node.left, sum, false);
+                dfs(node.right, sum, false);
+            }
+            dfs(node.left, sum - node.val, true);
+            dfs(node.right, sum - node.val, true);
+        }
     }
 }
