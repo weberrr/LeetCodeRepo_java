1312d79d5eec1817426494ede4c17efe756a974f
wangze <wangze0801@126.com>
2018-12-20 20:31:29 +0800

    7.整数反转 : 数学

---
+++ src/_7.java
@@ -0,0 +1,26 @@
+/**
+ * 整数反转
+ *
+ * @date 2018/12/20
+ */
+public class _7 {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _7().reverse(1534236469));
+    }
+}
