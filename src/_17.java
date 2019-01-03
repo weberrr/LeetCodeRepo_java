f5e3f900dad054ac0b2a35439800be3de32db3df
wangze <wangze0801@126.com>
2019-01-03 19:13:34 +0800

    17. 电话号码的字母组合 : 回溯问题

---
+++ src/_17.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 电话号码的字母组合
+ *
+ * @date 2019/1/3
+ */
+public class _17 {
+
+    //回溯
+    public List<String> letterCombinations(String digits) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(2, "abc");
+        map.put(3, "def");
+        map.put(4, "ghi");
+        map.put(5, "jkl");
+        map.put(6, "mno");
+        map.put(7, "pqrs");
+        map.put(8, "tuv");
+        map.put(9, "wxyz");
+        return letterCombinations(digits, new ArrayList<>(), map, "", 0);
+    }
+
+    private List<String> letterCombinations(String digits, List<String> list, HashMap<Integer, String> map, String sub, int index) {
+        if (index == digits.length()) {
+            if (sub.length() != 0) list.add(sub);
+        } else {
+            String temp = map.get(Integer.valueOf(digits.substring(index, index + 1)));
+            for (char c : temp.toCharArray()) {
+                letterCombinations(digits, list, map, sub + c, index + 1);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _17().letterCombinations("23"));
+    }
+}
