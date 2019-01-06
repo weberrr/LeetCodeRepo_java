36acde77cea58097d37c8e83599cec32169ea1f8
wangze <wangze0801@126.com>
2019-01-06 15:42:40 +0800

    25. k个一组翻转链表 ： 链表 递归

--- src/_25.java
+++ src/_25.java
@@ -6,21 +6,33 @@ import Utils.*;
  * @date 2019/1/5
  */
 public class _25 {
+
+    //递归
     public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode temp = head;
+        if (head == null || head.next == null || k == 1) return head;
+        ListNode countLen = head;
         int len = 0;
-        while (temp.next != null) {
-            temp = temp.next;
+        while (len < k && countLen != null) {
+            countLen = countLen.next;
             len++;
         }
-        if (len < k) {
-            return head;
-        } else {
-            for (int i = 0; i < len; i+=k) {
-
+        if (len == k) {
+            ListNode prev = null;
+            ListNode next = null;
+            ListNode cur = head;
+            int count = 0;
+            while (count < k && cur != null) {
+                next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+                count++;
             }
+            if (next != null) head.next = reverseKGroup(next, k);
+            return prev;
+        } else {
+            return head;
         }
-        return head;
     }
 
 }
