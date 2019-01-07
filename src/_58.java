f609eaabd8939676f12dfc4980e73a2987c1d1a4
wangze <wangze0801@126.com>
2019-01-07 21:19:56 +0800

    58. 最后一个单词的长度 : trim

--- src/_58.java
+++ src/_58.java
@@ -10,4 +10,20 @@ public class _58 {
         int index = s.lastIndexOf(' ');
         return s.substring(index + 1).length();
     }
+
+    public int lengthOfLastWord2(String s) {
+        if (s.length() == 0)
+            return 0;
+        int i = s.length() - 1;
+        int length = 0;
+
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
+
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            --i;
+        }
+        return length;
+    }
 }
