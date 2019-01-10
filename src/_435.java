b61af8615973e01a8c848f53c93fff3d33402abe
wangze <wangze0801@126.com>
2019-01-10 20:18:31 +0800

    435. 无重叠区间 : 贪心算法 + 优先队列

---
+++ src/_435.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+/**
+ * 435. 无重叠区间
+ */
+public class _435 {
+
+    public static class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+    }
+
+    //构建优先队列 使用两次循环
+    public int eraseOverlapIntervals(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        PriorityQueue<Interval> priorityQueue = new PriorityQueue<>(intervals.length, new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.end - o2.end;
+            }
+        });
+        for (Interval interval : intervals) {
+            priorityQueue.offer(interval);
+        }
+        int res = 0;
+        int end = Integer.MIN_VALUE;
+        while (!priorityQueue.isEmpty()) {
+            Interval cur = priorityQueue.poll();
+            if (cur.start >= end) {
+                end = cur.end;
+            } else {
+                res += 1;
+            }
+        }
+        return res;
+    }
+
+    //使用Array.sort 一次循环
+    public int eraseOverlapIntervals2(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) return 0;
+        Comparator<Interval> comparator = new Comparator<Interval>() {
+            @Override
+            public int compare(Interval o1, Interval o2) {
+                return o1.end - o2.end;
+            }
+        };
+        Arrays.sort(intervals, comparator);
+        int res = 0;
+        int end = Integer.MIN_VALUE;
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i].start >= end) end = intervals[i].end;
+            else res += 1;
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        Interval[] list = new Interval[]{new Interval(1, 2), new Interval(2, 3), new Interval(3, 4), new Interval(-100, -2), new Interval(5, 7)};
+        System.out.println(new _435().eraseOverlapIntervals(list));
+    }
+}
