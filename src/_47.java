5aea49daea1d1758ba6a4bad149e87a5f9194d39
wangze <wangze0801@126.com>
2019-01-13 18:33:33 +0800

    47. 全排列 II : 优化：添加符号位辅助

--- src/_47.java
+++ src/_47.java
@@ -41,6 +41,33 @@ public class _47 {
         nums[j] = temp;
     }
 
+    //最快的方法：设定符号位
+    public List<List<Integer>> permuteUnique2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums==null || nums.length==0) return res;
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            list.add(nums[i]);
+            dfs(nums,used,list,res);
+            used[i]=false;
+            list.remove(list.size()-1);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(new _47().permuteUnique(new int[]{1, 2, 1}));
     }
