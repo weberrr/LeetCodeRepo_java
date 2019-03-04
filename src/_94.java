0edb32e7f7583d269f71867d015f910ea9c9d6dd
wangze <wangze0801@126.com>
2019-03-04 18:12:05 +0800

    94. 二叉树的中序遍历 ： 栈

---
+++ src/_94.java
@@ -0,0 +1,31 @@
+import Utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94. 二叉树的中序遍历
+ *
+ * @date 2019/3/4
+ */
+public class _94 {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                res.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return res;
+    }
+
+}
